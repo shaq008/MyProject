7a3ac1050b4dd42bb11d7b992f4f18e5e5309fe2
shaq008 <shayak8@gmail.com>
2022-06-03 16:03:57 +0530

    branch 'issue1' first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 
 public class Main {
+	static int countDigits(int number){
+		if(number>=1 && number<=9)
+			return 1;
+		else if(number>=10 && number<=99)
+			return 2;
+		else if(number>=100 && number<=999)
+			return 3;
+		else
+			return -1;
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Shaq");
